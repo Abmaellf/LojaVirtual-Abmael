b9edc43eccc631302027086cf22d4a216ba4f1a0
FerrerSistemas <abmael.ninha@gmail.com>
2020-07-26 22:19:28 -0400

    63 - Email HTML

--- src/main/java/com/lojavirtualabmael/service/EmailService.java
+++ src/main/java/com/lojavirtualabmael/service/EmailService.java
@@ -1,12 +1,17 @@
 package com.lojavirtualabmael.service;
 
-import com.lojavirtualabmael.domain.Pedido;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
+
+import com.lojavirtualabmael.domain.Pedido;
 
 
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
-
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
