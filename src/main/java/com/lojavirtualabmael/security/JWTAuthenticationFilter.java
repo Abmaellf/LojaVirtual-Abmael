229b0529141a56dd97ea3d7268d3b23250c22fc8
FerrerSistemas <abmael.ninha@gmail.com>
2020-12-06 14:38:35 -0400

    93-Expondo o header Authorization-problema de Cors

--- src/main/java/com/lojavirtualabmael/security/JWTAuthenticationFilter.java
+++ src/main/java/com/lojavirtualabmael/security/JWTAuthenticationFilter.java
@@ -62,6 +62,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		res.addHeader("Authorization", "Bearer "+ token);
+		res.addHeader("access-control-expose-headers", "Authorization"); 
+		
 
 	}
 	
